3c29c6cf8b46f5c80ed0063227c61e3b7de1369b
HARIVOLA Andy <hei.andy.8@gmail.com>
2022-09-14 16:50:06 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Connexion from "./components/login/connexion";
+
+function App() {
+  return (
+    <>
+        <Connexion/>
+    </>
+  );
+}
+
+export default App;
